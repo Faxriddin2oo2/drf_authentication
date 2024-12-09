8b7cc71cdcbf69eabc5d9ad475635b4efc25dcc3
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-09 19:30:42 +0500

    add ChangeUserInformationView in views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from rest_framework.response import Response
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from users.serializers import SignUpSerializer
+from users.serializers import SignUpSerializer, ChangeUserInformation
 from .models import User
 
 
@@ -9,3 +11,30 @@ class CreateUserView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = SignUpSerializer
+
+
+class ChangeUserInformationView(UpdateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    serializer_class = ChangeUserInformation
+    http_method_names = ['patch', 'put']
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        super(ChangeUserInformationView, self).update(request, *args, **kwargs)
+        data = {
+            'success' : True,
+            'message' : "User updated successfully",
+            'auth_status' : self.request.user.auth_status,
+        }
+        return Response(data, status=200)
+
+    def partial_update(self, request, *args, **kwargs):
+        super(ChangeUserInformationView, self).partial_update(request, *args, **kwargs)
+        data = {
+            'success': True,
+            'message': "User updated successfully",
+            'auth_status': self.request.user.auth_status
+        }
+        return Response(data, status=200)
