f583422e00500ab51e77f04d40dd94855435eaf9
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-12 20:44:09 +0500

    Wrote VerifyAPIView and GetNewVerification in views.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from users.views import CreateUserView, ChangeUserInformationView, ChangeUserPhotoView
-
+from users.views import CreateUserView, ChangeUserInformationView, ChangeUserPhotoView,\
+    VerifyAPIView, GetNewVerification
 
 urlpatterns = [
     path('signup/', CreateUserView.as_view()),
+    path('verify/', VerifyAPIView.as_view()),
+    path('new-verify/', GetNewVerification.as_view()),
     path('change-user/', ChangeUserInformationView.as_view()),
     path('change-user-photo/', ChangeUserPhotoView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from pickle import NEWOBJ
+
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView, UpdateAPIView
@@ -5,13 +9,79 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from users.serializers import SignUpSerializer, ChangeUserInformation, ChangeUserPhotoSerializer
-from .models import User
+from .models import User, CODE_VERIFIED, NEW, VIA_PHONE
+from .utility import send_email
 
 
 class CreateUserView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = SignUpSerializer
+
+
+class VerifyAPIView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        code = self.request.data.get('code')
+
+        self.check_verify(user, code)
+        return Response(
+            data={
+                "success": True,
+                "auth_status": user.auth_status,
+                "access": user.token()['access'],
+                "refresh": user.token()['refresh_token']
+            }
+        )
+
+    @staticmethod
+    def check_verify(user, code):
+        verifies = user.verify_codes.filter(expiration_time__gte=datetime.now(), code=code, is_confirmed=False)
+        if not verifies.exists():
+            data = {
+                "message": "Tasdiqlash kodingiz xato yoki eskirgan"
+            }
+            raise ValidationError
+        else:
+            verifies.update(is_confirmed=True)
+        if user.auth_type == NEW:
+            user.auth_status = CODE_VERIFIED
+            user.save()
+        return True
+
+
+class GetNewVerification(APIView):
+    permission_classes = [IsAuthenticated, ]
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        self.check_verification(user)
+        if user.auth_type == VIA_PHONE:
+            code = user.create_verify_code(VIA_PHONE)
+            send_email(user.phone_number, code)
+        else:
+            data = {
+                "message" : "Email yoki telefon raqami noto'g'ri"
+            }
+            raise ValidationError(data)
+
+        return Response(
+            {
+                "success" : True,
+                "message" : "Tasdiqlash kodingiz qaytadan jo'natildi."
+            }
+        )
+
+
+    @staticmethod
+    def check_verification(user):
+        verifies = user.verify_code.filter(expiration_time__gte=datetime.now(), is_confirmed=False)
+        if verifies.exists():
+            data = {
+                "message" : "Kodingiz hali ishlatish uchun yaroqli. Biroz kutib turing"
+            }
+            raise ValidationError(data)
 
 
 class ChangeUserInformationView(UpdateAPIView):
